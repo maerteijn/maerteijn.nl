6b1e5869071728404c60a5fc3a4090bed31138d7
Martijn Jacobs <maerteijn@gmail.com>
2020-01-14 15:35:42 +0100

    Added vue-router and some placeholder pages

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,22 @@
 <template>
 <div class="main">
-  Welcome to maerteijn's vue.js website
+  <h1>Welcome to maerteijn's vue.js website</h1>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </div>
 </template>
 
 <script>
 export default {
-  components: {
-  }
 };
 </script>
 
 <style scoped>
-.main {
+h1 {
   color: #FB7516;
   font-size: 5.75vw;
 }
-
-
 </style>
