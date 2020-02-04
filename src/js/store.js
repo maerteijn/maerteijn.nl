b4aab4b12827e911fb6545f01c47448608d5009a
Martijn Jacobs <maerteijn@gmail.com>
2020-02-04 12:44:17 +0100

    Use a simple store fetched by json for the content

---
+++ src/js/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+export const state = Vue.observable({
+  content: {}
+});
+
+export const actions = {
+  fetchJsonContent() {
+    return axios
+      .get('/data.json')
+      .then((response) => {
+        state.content = response.data
+      }
+    )
+  }
+}
