c5424ee3fd687e1839d733d7918810de29245517
Martijn Jacobs <maerteijn@gmail.com>
2020-04-26 19:40:28 +0200

    Moved router specific registration to router.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from "./app"
 import router from "./js/router"
-import './scss/main.scss'
 
-Vue.use(VueRouter)
+import './scss/main.scss'
 
 new Vue({
   router: router,
