cde57740de674fe31bb154b905576fd81a128ab9
Martijn Jacobs <maerteijn@gmail.com>
2020-04-26 21:21:08 +0200

    Added a basic layout and use it on the homepage

---
+++ src/js/layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <main class="main">
+    <div class="columns">
+      <div class="column is-two-fifths left">left</div>
+      <div class="column right"><slot name="default"></slot></div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.main {
+  display: grid;
+  height: 100vh;
+  overflow:hidden;
+  width: 100vw;
+
+  .left {
+    box-shadow: 0 0 5px 0 variables.$drop-shadow-color;
+    z-index:1;
+  }
+
+  .right {
+    background-color: variables.$background-color;
+    color: variables.$font-color;
+    height: 100%;
+    overflow: scroll;
+  }
+}
+
+@media screen and (max-width: bulma.$tablet) {
+  .main {
+    .left {
+      box-shadow: 0 0 15px 0 variables.$drop-shadow-color;
+      position:relative
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from "./app"
 import router from "./js/router"
 
+// register global components
+import Default from "./js/layouts/default"
+Vue.component("default-layout", Default)
+
 import './scss/main.scss'
 
 new Vue({
