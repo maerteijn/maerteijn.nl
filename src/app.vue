e4576b9805966051bb0e808ad66e4c7db67ff31f
Martijn Jacobs <maerteijn@gmail.com>
2020-04-26 19:50:40 +0200

    Cleanup of the app component

--- src/app.vue
+++ src/app.vue
@@ -1,23 +1,9 @@
 <template>
-<div class="main">
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view></router-view>
-
-</div>
+  <router-view :key="$route.fullPath"></router-view>
 </template>
 
 <script>
-import { state, actions } from "./js/store"
-
 export default {
-   computed: {
-      content() {
-        return state.content
-      }
-    },
     created() {
       actions.fetchJsonContent()
     }
