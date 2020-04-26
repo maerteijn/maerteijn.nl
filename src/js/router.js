aabb05b71738f16b9a470aec52bd745dcd51707c
Martijn Jacobs <maerteijn@gmail.com>
2020-04-26 19:40:28 +0200

    Moved router specific registration to router.js

--- src/js/router.js
+++ src/js/router.js
@@ -1,7 +1,10 @@
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from "./pages/home"
 import About from "./pages/about"
+
+Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
