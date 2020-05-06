d74f7b7847d198fe783a53fe8f9f31876307e9e9
Martijn Jacobs <maerteijn@gmail.com>
2020-05-06 22:26:21 +0200

    Added unittests for the markdown renderer

---
+++ tests/unit/test_markdown.js
@@ -0,0 +1,21 @@
+import { assert } from "chai"
+
+import { renderMarkdown } from "../../src/js/markdown"
+
+const markdown = `
+# Header
+
+This is a paragraph
+`
+describe("Test markdown", () => {
+  it("The markdown renderer works as expected", () => {
+    const html = renderMarkdown(markdown)
+    assert.include(html, '<h1 id="header">Header</h1>')
+    assert.include(html, "<p>This is a paragraph</p>")
+  })
+
+  it("The markdown renderer support Emojis", () => {
+    const html = renderMarkdown(":smile:")
+    assert.equal(html, "<p>😄</p>")
+  })
+})
