6ee14d906390b8ab3ff82f483b51189b37ff490e
Martijn Jacobs <maerteijn@gmail.com>
2020-05-11 22:39:43 +0200

    Don't register VueRouter in router.js as we want to test without it

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue"
+import VueRouter from "vue-router"
 import App from "./app"
 import router from "./js/router"
 
@@ -9,6 +10,9 @@ Vue.component("default-layout", Default)
 // register state as a global prototype
 import { state } from "./js/store"
 Vue.prototype.$state = state
+
+// register the vue router plugin on the global vue instance
+Vue.use(VueRouter)
 
 import "./scss/main.scss"
 
