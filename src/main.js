a086c66e94d4fd246ba4706322f7279cba4c93a8
Martijn Jacobs <maerteijn@gmail.com>
2020-05-24 21:51:47 +0200

    Manually add the Object.assign polyfill for IE 11

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// We add the Object.assign polyfill for IE11 to function with Vue.js
+import "core-js/stable/object/assign"
 import Vue from "vue"
 import VueRouter from "vue-router"
 import App from "./app"
