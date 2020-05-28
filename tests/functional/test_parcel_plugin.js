9ba8f9ad4bb580baf16051cc1056e48d614e0832
Martijn Jacobs <maerteijn@gmail.com>
2020-05-28 15:34:09 +0200

    Added test to check if the sitemap is generated

---
+++ tests/functional/test_parcel_plugin.js
@@ -0,0 +1,15 @@
+import { assert } from "chai"
+import { promises as fs } from "fs"
+import path from "path"
+
+describe("Test parcel plugin", () => {
+  it("A parcel plugin writes a sitemap.xml file with each build", () => {
+    return fs
+      .readFile("./dist/test/sitemap.xml", { encoding: "utf-8" })
+      .then((data) => {
+        assert.include(data, '<?xml version="1.0" encoding="UTF-8"?>')
+        // there should be 8 url tags in the sitemap
+        assert.lengthOf(data.match(/\<url\>/g) || [], 8)
+      })
+  })
+})
