29faf13e143a015340699b0689b3f5211de7f9ed
Martijn Jacobs <maerteijn@gmail.com>
2020-05-28 15:33:58 +0200

    Added parcel pluginhook to generate a sitemap.xml

---
+++ src/parcel-plugin/main.js
@@ -0,0 +1,51 @@
+import path from "path"
+import { promises as fs } from "fs"
+import config from "parcel-bundler/src/utils/config"
+import { SitemapStream, streamToPromise } from "sitemap"
+
+import { validator } from "../js/schema"
+
+const loadJSON = async (json_path) => {
+  const json = await fs.readFile(json_path)
+  const parsed = JSON.parse(json)
+
+  // Check if the loaded json is valid
+  if (!validator(parsed)) {
+    const error = validator.errors[0]
+    throw `Site data does not validate ${error.dataPath}: ${error.message}`
+  }
+  return parsed
+}
+
+const generate_sitemap = (hostname, pages) => {
+  const stream = new SitemapStream({ hostname: hostname })
+  pages.forEach((page) => stream.write({ url: page.path, changefreq: "daily" }))
+  stream.end()
+  return streamToPromise(stream).then((data) => data.toString())
+}
+
+export default (bundler) => {
+  bundler.on("bundled", async (bundle) => {
+    const package_json_path = await config.resolve(__dirname, ["package.json"])
+    const package_json = await config.load(package_json_path, ["package.json"])
+
+    try {
+      const site_json = await loadJSON(
+        path.join(
+          path.dirname(package_json_path),
+          package_json.sitemap_settings.site_json
+        )
+      )
+
+      const sitemap_xml = await generate_sitemap(
+        package_json.sitemap_settings.hostname,
+        site_json.pages.filter((page) => page.settings.show_in_menu)
+      )
+
+      const dest_path = path.join(path.dirname(bundle.name), "sitemap.xml")
+      fs.writeFile(dest_path, sitemap_xml)
+    } catch (e) {
+      console.error(e)
+    }
+  })
+}
