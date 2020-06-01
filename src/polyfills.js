c750b6f347f5a9f5b5d4610d5f5bb86cb24fe9bf
Martijn Jacobs <maerteijn@gmail.com>
2020-06-01 14:52:15 +0200

    Updated polyfill comment

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,7 @@
-// We load these polyfills to make all magic work with IE11
+// We load these polyfills to make all magic work with IE11. This is needed
+// because parcel only transpile the local sources with babel. Packages
+// from node_modules (vue.js etc) will not be transpiled when their
+// browserslist configuration is not set explicitly.
 import "core-js/stable/object/assign"
 import "core-js/stable/object/to-string"
 import "core-js/stable/array/iterator"
