420e220c31dad3658a4a7d10f55ad9d07b7b8819
Martijn Jacobs <maerteijn@gmail.com>
2020-06-08 15:01:08 +0200

    Create routerlinks for internal links, added tests

--- tests/unit/components/test_markdown-viewer.js
+++ tests/unit/components/test_markdown-viewer.js
@@ -1,12 +1,14 @@
 import { assert } from "chai"
+import sinon from "sinon"
 
 import modules from "../../../dist/test"
-
 import { createComponent } from "../../utils"
 
 describe("Markdown viewer component", () => {
-  it("We can initialize a Markdown component", () => {
-    const wrapper = createComponent(modules.MarkdownViewer)
+  it("We can initialize a Markdown component", function () {
+    const wrapper = createComponent(modules.MarkdownViewer, {
+      content: "hi!",
+    })
     assert.equal(wrapper.vm.$options.name, "markdown-viewer")
   })
 
@@ -22,5 +24,39 @@ describe("Markdown viewer component", () => {
       content: "# Hello!",
     })
     assert.include(wrapper.html(), '<h1 id="hello">Hello!</h1>')
+  })
+
+  it("The component should call the createImageViewer method", function () {
+    modules.MarkdownViewer.methods.createImageViewer = sinon.spy(
+      modules.MarkdownViewer.methods.createImageViewer
+    )
+
+    // no image? then the image viewer should not be called
+    createComponent(modules.MarkdownViewer, { content: "#Hi!" })
+    assert.isFalse(modules.MarkdownViewer.methods.createImageViewer.called)
+
+    // when there is, it should cal it
+    const wrapper = createComponent(modules.MarkdownViewer, {
+      content: "![Photo](/images/photo-small.jpg#left =120x*)",
+    })
+    assert.isTrue(modules.MarkdownViewer.methods.createImageViewer.called)
+    sinon.restore()
+  })
+
+  it("The component calls the createRouterLink function for internal links", function () {
+    modules.MarkdownViewer.methods.createRouterLink = sinon.spy(
+      modules.MarkdownViewer.methods.createRouterLink
+    )
+
+    // no link? then the create router link method should not be called
+    createComponent(modules.MarkdownViewer, { content: "#Hi!" })
+    assert.isFalse(modules.MarkdownViewer.methods.createRouterLink.called)
+
+    // when there is, it should cal it
+    const wrapper = createComponent(modules.MarkdownViewer, {
+      content: "[My link](/to/an/internal/link)",
+    })
+    assert.isTrue(modules.MarkdownViewer.methods.createRouterLink.called)
+    sinon.restore()
   })
 })
