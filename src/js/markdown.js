50faf433e5771d9cf0f171dfacdb09948073c1ab
Martijn Jacobs <maerteijn@gmail.com>
2020-06-08 15:01:08 +0200

    Create routerlinks for internal links, added tests

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -5,6 +5,7 @@ converter.setOption("parseImgDimensions", true)
 converter.setOption("emoji", true)
 converter.setOption("tables", true)
 converter.setFlavor("github")
+converter.setOption("openLinksInNewWindow", true)
 
 export const renderMarkdown = (content) => {
   return converter.makeHtml(content)
