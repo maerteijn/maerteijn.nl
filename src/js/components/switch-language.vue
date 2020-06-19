b924176d6bffecbfaa9061af4be0f2430a9c04a3
Martijn Jacobs <maerteijn@gmail.com>
2020-06-19 17:07:29 +0200

    Refactor the language selector to show the language abbreviations
    And removed some clutter from the store

--- src/js/components/switch-language.vue
+++ src/js/components/switch-language.vue
@@ -1,39 +1,51 @@
 <template>
   <div class="switch-language" v-if="canSwitch">
-    <a v-bind:title="switchTo">
-      <div
-        id="switch-language"
-        class="icon"
-        v-html="icon('fas-flag')"
-        v-on:click="switchLanguage"
-      ></div>
-    </a>
+    <template v-for="language in availableLanguages">
+      <span v-if="language != currentLanguage"
+        ><a
+          id="switch-language"
+          title="Switch language"
+          v-on:click="switchLanguage(language)"
+          >{{ language }}</a
+        ></span
+      >
+    </template>
   </div>
 </template>
 
 <script>
-import { getIcon } from "../icons"
-import { getters } from "../store"
+import { getters, state } from "../store"
 
 export default {
   name: "switch-language",
   computed: {
-    switchTo() {
-      return getters.getSwitchableLanguage(this.$route.path)
+    availableLanguages() {
+      return Object.keys(state.site.site_settings.languages)
+    },
+    currentLanguage() {
+      return getters.getCurrentLanguage(this.$route.path)
     },
     canSwitch() {
-      return getters.getPagesForLanguage(this.switchTo).length > 0
+      return this.availableLanguages.length > 0
     },
   },
   methods: {
-    icon: getIcon,
-    switchLanguage() {
-      window.localStorage.language = this.switchTo
+    switchLanguage(language) {
+      console.log
+      window.localStorage.language = language
       const translated_page =
-        getters.getTranslatedPages(this.$route.path, this.switchTo)[0] ||
-        getters.getPagesForLanguage(this.switchTo)[0]
+        getters.getTranslatedPages(this.$route.path, language)[0] ||
+        getters.getPagesForLanguage(language)[0]
       this.$router.push(translated_page.path)
     },
   },
 }
 </script>
+
+<style lang="scss">
+.switch-language {
+  span {
+    padding: 0 0.2rem;
+  }
+}
+</style>
