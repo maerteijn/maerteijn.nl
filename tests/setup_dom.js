7d11e948980b23b5d620968ed5d17f9b6938221f
Martijn Jacobs <maerteijn@gmail.com>
2020-07-07 12:19:28 +0200

    Added a description meta tag

--- tests/setup_dom.js
+++ tests/setup_dom.js
@@ -1,1 +1,14 @@
-require("jsdom-global")(undefined, { url: "http://localhost" })
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>maerteijn</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+  </head>
+  <body>
+  </body>
+</html>
+`
+require("jsdom-global")(html, { url: "http://localhost" })
