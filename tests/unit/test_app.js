ad6c25a56bda15ab593c59a6951db061cc0a0370
Martijn Jacobs <maerteijn@gmail.com>
2021-08-01 16:52:56 +0200

    Re-enable main App component unit tests

--- tests/unit/test_app.js
+++ tests/unit/test_app.js
@@ -1,9 +1,8 @@
 import { assert } from "chai"
 import sinon from "sinon"
 
-import { createWrapper } from "@vue/test-utils"
+import { mount } from "@vue/test-utils"
 import App from "@/app"
-// import modules from "../../dist/test"
 
 import {
   createComponentWithoutRouter,
@@ -29,35 +28,25 @@ describe("App component", () => {
     assert.equal(wrapper.vm.$options.name, "app")
   })
 
-  // it("The main App component renders the router view when no error exists", () => {
-  //   const wrapper = createComponentWithoutRouter(modules.App, { path: "/" })
-  //   assert.equal(wrapper.html(), "<router-view-stub></router-view-stub>")
-  // })
+  it("The main App component renders the router view when no error exists", () => {
+    const wrapper = createComponentWithoutRouter(App, { path: "/" })
+    assert.equal(wrapper.html(), "<router-view-stub></router-view-stub>")
+  })
 
-  // it("The main App component renders an error message when it exists", () => {
-  //   const wrapper = createComponentWithoutRouter(modules.App, { path: "/" })
-  //   store.state.error = "Oh noo!"
-  //   return wrapper.vm.$nextTick().then(() => {
-  //     assert.isTrue(wrapper.find(".error-message").exists())
-  //   })
-  // })
+  it("The main App component renders an error message when it exists", () => {
+    const wrapper = createComponentWithoutRouter(App, { path: "/" })
+    store.state.error = "Oh noo!"
+    return wrapper.vm.$nextTick().then(() => {
+      assert.isTrue(wrapper.find(".error-message").exists())
+    })
+  })
 
-  // it("The main App component renders an error message when axios returns an error", function () {
-  //   this.stub = mock_axios_error()
-  //   const wrapper = createComponentWithoutRouter(modules.App, { path: "/" })
-  //   return waitForPromises().then(() => {
-  //     assert.isTrue(wrapper.find(".error-message").exists())
-  //     assert.include(wrapper.html(), "Invalid JSON response when requesting")
-  //   })
-  // })
-
-  // it("The main App component listens for emitted error events and calls the handleError method", function () {
-  //   const wrapper = createComponentWithoutRouter(modules.App, { path: "/" })
-  //   wrapper.vm.handleError = sinon.spy(wrapper.vm.handleError)
-  //   wrapper.vm.$root.$emit("error", "Something went wrong!")
-  //   assert.isTrue(wrapper.vm.handleError.called)
-
-  //   const root = createWrapper(wrapper.vm.$root)
-  //   assert.property(root.emitted(), "error")
-  // })
+  it("The main App component renders an error message when axios returns an error", function () {
+    this.stub = mock_axios_error()
+    const wrapper = createComponentWithoutRouter(App, { path: "/" })
+    return waitForPromises().then(() => {
+      assert.isTrue(wrapper.find(".error-message").exists())
+      assert.include(wrapper.html(), "Invalid JSON response when requesting")
+    })
+  })
 })
