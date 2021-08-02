1cf528b28e700e0b22c4014eebecfa75a7ffe249
Martijn Jacobs <maerteijn@gmail.com>
2021-08-02 11:35:03 +0200

    Uncomment the component utils  tests

--- tests/unit/components/test_utils.js
+++ tests/unit/components/test_utils.js
@@ -1,20 +1,20 @@
 import { assert } from "chai"
 import Vue from "vue"
 
-// import getContentComponent from "@/js/components/utils"
-// import MarkdownViewer from "@/js/components/markdown-viewer"
-// import LastUpdated from "@/js/components/last-updated"
+import getContentComponent from "@/js/components/utils"
+import MarkdownViewer from "@/js/components/markdown-viewer"
+import LastUpdated from "@/js/components/last-updated"
 
 describe("Test component utils", () => {
-  // it("The getContentComponent returns undefined when the contentcomponent is not known", () => {
-  //   assert.isUndefined(getContentComponent("unknown"))
-  // })
-  // it("The getContentComponent returns the MarkdownViewer component", () => {
-  //   const component = getContentComponent("MarkdownViewer")
-  //   assert.equal(component, MarkdownViewer)
-  // })
-  // it("The getContentComponent returns the LastUpdated component", () => {
-  //   const component = getContentComponent("LastUpdated")
-  //   assert.equal(component, LastUpdated)
-  // })
+  it("The getContentComponent returns undefined when the contentcomponent is not known", () => {
+    assert.isUndefined(getContentComponent("unknown"))
+  })
+  it("The getContentComponent returns the MarkdownViewer component", () => {
+    const component = getContentComponent("MarkdownViewer")
+    assert.equal(component, MarkdownViewer)
+  })
+  it("The getContentComponent returns the LastUpdated component", () => {
+    const component = getContentComponent("LastUpdated")
+    assert.equal(component, LastUpdated)
+  })
 })
