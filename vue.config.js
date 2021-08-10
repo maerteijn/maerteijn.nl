42dd43973590b15be99010aa5dbf72f3ceb01926
Martijn Jacobs <maerteijn@gmail.com>
2021-08-10 14:49:58 +0200

    Removed Vue2 compat config and package and updated jsdom

--- vue.config.js
+++ vue.config.js
@@ -16,19 +16,5 @@ module.exports = {
         .use('null-loader')
         .loader('null-loader');
     }
-    config.resolve.alias.set('vue', '@vue/compat')
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
   }
 }
