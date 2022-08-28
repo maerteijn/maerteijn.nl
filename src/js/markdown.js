37ab6dbb7af9b622f55ddd617c82fba017750340
Martijn Jacobs <maerteijn@gmail.com>
2022-08-28 22:28:38 +0200

    Enable code highlighting in the markdown viewer

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,6 +1,15 @@
 import showdown from "showdown"
+import showdownHighlight from "showdown-highlight"
 
-const converter = new showdown.Converter()
+import hljs from "highlight.js/lib/core"
+import python from "highlight.js/lib/languages/python"
+hljs.registerLanguage("python", python)
+
+import "highlight.js/styles/monokai-sublime.css"
+
+const converter = new showdown.Converter({
+  extensions: [showdownHighlight({ pre: true })],
+})
 converter.setOption("parseImgDimensions", true)
 converter.setOption("emoji", true)
 converter.setOption("tables", true)
