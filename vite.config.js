4f51f729dc5f0dbd27d6656162176be092635338
Martijn Jacobs <maerteijn@gmail.com>
2024-06-16 10:17:20 +0200

    Update bulma to v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @use "bulma/sass/utilities/initial-variables.sass" as bulma;
+          @use "bulma/sass/utilities/initial-variables.scss" as bulma;
           @use "./src/scss/variables.scss" as variables;
           @use "./src/scss/animation.scss" as *;
           `,
